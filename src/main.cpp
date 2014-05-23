89dd92d102b5a8532fb468cc7bb94e526bc1e197
Kristof Provost <kristof@codepro.be>
2014-05-23 23:31:25 +0200

    Argument parsing
    For now we have one argument: the interface to monitor on.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <QApplication>
+#include <QStringList>
+#include <iostream>
+
+void usage(const QString &appName)
+{
+    std::cerr << "Usage: " << appName.toStdString() << " <wifi interface>"
+        << std::endl;
+}
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
+    QStringList args = app.arguments();
+
+    QString appName = args.at(0);
+    args.removeAt(0);
+
+    if (args.size() < 1)
+    {
+        usage(appName);
+        return 1;
+    }
+
+    QString interface = args.at(0);
 
     return app.exec();
 }
