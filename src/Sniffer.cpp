0e8cbb2ceaa15882c54a3bc3d4d20b98d7e9c08c
Kristof Provost <kristof@codepro.be>
2014-05-27 23:41:24 +0200

    [Sniffer] Open the pcap interface on creation
    And close it again on destruction, obviously.

--- src/Sniffer.cpp
+++ src/Sniffer.cpp
@@ -1,19 +1,54 @@
 #include "Sniffer.h"
+#include <assert.h>
+#include <iostream>
+
+const int Sniffer::SNAPLEN = 1500;
 
 SnifferP_t Sniffer::create(const QString &interface)
 {
     SnifferP_t sniff(new Sniffer(interface));
+
+    if (! sniff->open())
+        sniff.clear();
+
     return sniff;
 }
 
 Sniffer::Sniffer(const QString &interface)
     : QObject(),
-      m_interface(interface)
+      m_interface(interface),
+      m_pcap(NULL)
 {
 
 }
 
 Sniffer::~Sniffer()
 {
+    if (m_pcap)
+        close();
+}
 
+bool Sniffer::open()
+{
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    assert(! m_pcap);
+
+    m_pcap = pcap_open_live(m_interface.toStdString().c_str(), SNAPLEN,
+            1, 1, errbuf);
+    if (! m_pcap)
+    {
+        std::cerr << "Failed to open pcap interface " << m_interface.toStdString()
+            << ": " << errbuf;
+        return false;
+    }
+
+    return true;
+}
+
+void Sniffer::close()
+{
+    assert(m_pcap);
+    pcap_close(m_pcap);
+    m_pcap = NULL;
 }
--- src/Sniffer.h
+++ src/Sniffer.h
@@ -2,6 +2,7 @@
 
 #include <QObject>
 #include <QSharedPointer>
+#include <pcap/pcap.h>
 
 class Sniffer;
 typedef QSharedPointer<Sniffer> SnifferP_t;
@@ -17,6 +18,11 @@ public:
 
 private:
     Sniffer(const QString &interface);
+    bool open();
+    void close();
 
     const QString &m_interface;
+    pcap_t *m_pcap;
+
+    static const int SNAPLEN;
 };
