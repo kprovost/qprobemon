0e095ec9091d22e496aa63834755437dd89933c0
Kristof Provost <kristof@codepro.be>
2014-05-29 17:44:58 +0200

    [ProbeRequestFilter] Parse the radiotap header
    We have to include our own definition of the radiotap header because
    unfortunately there doesn't appear to be a package with the definition.
    
    Rather than messing around with unpackaged libraries it's easier to just
    include the header. We're only really interested in the length anyway,
    so we can parse the rest of the captured data.

--- src/ProbeRequestFilter.cpp
+++ src/ProbeRequestFilter.cpp
@@ -1,4 +1,13 @@
 #include "ProbeRequestFilter.h"
+#include <assert.h>
+#include <stdint.h>
+
+struct ieee80211_radiotap_header {
+    uint8_t        it_version;     /* set to 0 */
+    uint8_t        it_pad;
+    uint16_t       it_len;         /* entire length */
+    uint32_t       it_present;     /* fields present */
+} __attribute__((__packed__));
 
 ProbeRequestFilter::ProbeRequestFilter()
     : QObject()
@@ -8,5 +17,8 @@ ProbeRequestFilter::ProbeRequestFilter()
 
 void ProbeRequestFilter::received(PacketP_t packet)
 {
+    const struct ieee80211_radiotap_header *rh =
+        (const struct ieee80211_radiotap_header*)packet->getData().data();
 
+    assert(rh->it_version == 0);
 }
