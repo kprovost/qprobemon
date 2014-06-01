32c4708f259af05c69624514d3cd30cf87614640
Kristof Provost <kristof@codepro.be>
2014-06-01 14:37:17 +0200

    [MacAddress] Get manufacturer based on the MAC address
    We can now figure out the manufacturer of a device based on the mac
    address.
    
    This is a reasonably simplistic implementation, and it won't handle
    cases where the manufacturer prefix isn't exectly three bytes.

--- src/MacAddress.cpp
+++ src/MacAddress.cpp
@@ -1,5 +1,12 @@
 #include "MacAddress.h"
+
+#include <QFile>
+#include <QStringList>
+#include <QTextStream>
+#include <iostream>
 #include <assert.h>
+
+const QString MacAddress::MANUF_FILE = "/usr/share/wireshark/manuf";
 
 MacAddress::MacAddress(const quint8 *mac)
 {
@@ -19,4 +26,50 @@ QString MacAddress::toString() const
         .arg(QString::number((unsigned char)m_mac[3], 16))
         .arg(QString::number((unsigned char)m_mac[4], 16))
         .arg(QString::number((unsigned char)m_mac[5], 16));
+}
+
+QString MacAddress::getOUID() const
+{
+    assert(m_mac.size() == 6);
+
+    return QString("%1:%2:%3")
+        .arg(QString::number((unsigned char)m_mac[0], 16))
+        .arg(QString::number((unsigned char)m_mac[1], 16))
+        .arg(QString::number((unsigned char)m_mac[2], 16));
+}
+
+QString MacAddress::getManufacturer() const
+{
+    QFile f(MANUF_FILE);
+
+    if (! f.open(QIODevice::ReadOnly))
+    {
+        std::cerr << "Failed to open file " << MANUF_FILE.toStdString()
+            << std::endl;
+        return "Unknown";
+    }
+
+    QString ouid = getOUID().toUpper();
+
+    QTextStream in(&f);
+    QString line;
+    while (1)
+    {
+        line = in.readLine();
+        if (line.isNull())
+            break;
+
+        if (line.startsWith("#"))
+            continue;
+
+        if (line.startsWith(ouid))
+        {
+            QStringList fields = line.split(QRegExp("\t|\\s+"));
+
+            assert(fields.size() >= 2);
+            return fields[1];
+        }
+    }
+
+    return "Unknown";
 }
--- src/MacAddress.h
+++ src/MacAddress.h
@@ -10,7 +10,11 @@ public:
     MacAddress(const quint8 *mac);
 
     QString toString() const;
+    QString getOUID() const;
+    QString getManufacturer() const;
 
 private:
     QByteArray m_mac;
+
+    static const QString MANUF_FILE;
 };
