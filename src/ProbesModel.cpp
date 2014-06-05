9b040d1791a695bc2e6830f2de90ded8b1dc6973
Kristof Provost <kristof@codepro.be>
2014-06-05 23:31:18 +0200

    [ProbesModel] Move method implementation to the .cpp file
    As we do for all other classes...

--- src/ProbesModel.cpp
+++ src/ProbesModel.cpp
@@ -13,6 +13,77 @@ ProbesModel::~ProbesModel()
 {
 
 }
+QModelIndex ProbesModel::index(int row, int column, const QModelIndex & parent) const
+{
+    if (parent.isValid())
+    {
+        if (parent.internalId() != -1)
+            return QModelIndex();
+
+        return createIndex(row, column, parent.row());
+    }
+
+    return createIndex(row, column, -1);
+}
+
+QModelIndex ProbesModel::parent(const QModelIndex &index) const
+{
+    if (index.internalId() == -1)
+        return QModelIndex();
+
+    return createIndex(index.internalId(), 0, -1);
+}
+
+int ProbesModel::rowCount(const QModelIndex & parent) const
+{
+    if (! parent.isValid())
+        return m_store.size();
+
+    const MacAddress &mac = m_store.get(parent.internalId());
+    const StationPtr_t station = m_store.getStation(mac);
+
+    return station->getSSIDcount();
+}
+
+int ProbesModel::columnCount(const QModelIndex & parent) const
+{
+    if (parent.isValid())
+        return 1;
+
+    return 2;
+}
+
+QVariant ProbesModel::data(const QModelIndex & index, int role) const
+{
+    if (role != Qt::DisplayRole)
+        return QVariant();
+
+    if (index.internalId() == -1)
+    {
+        const MacAddress &mac = m_store.get(index.row());
+        if (index.column() == 0)
+        {
+            return QVariant(mac.toString());
+        }
+        else if (index.column() == 1)
+        {
+            return QVariant(mac.getManufacturer());
+        }
+        else
+        {
+            assert(false);
+        }
+    }
+    else
+    {
+        if (index.column() != 0)
+            return QVariant();
+
+        const MacAddress &mac = m_store.get(index.internalId());
+        const StationPtr_t station = m_store.getStation(mac);
+        return QVariant(station->getSSID(index.row()));
+    }
+}
 
 QVariant ProbesModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
--- src/ProbesModel.h
+++ src/ProbesModel.h
@@ -12,78 +12,11 @@ public:
     ProbesModel(ProbeStore &store);
     virtual ~ProbesModel();
 
-    QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const
-    {
-        if (parent.isValid())
-        {
-            if (parent.internalId() != -1)
-                return QModelIndex();
-
-            return createIndex(row, column, parent.row());
-        }
-
-        return createIndex(row, column, -1);
-    }
-
-    QModelIndex parent(const QModelIndex &index) const
-    {
-        if (index.internalId() == -1)
-            return QModelIndex();
-
-        return createIndex(index.internalId(), 0, -1);
-    }
-
-    int rowCount(const QModelIndex & parent = QModelIndex()) const
-    {
-        if (! parent.isValid())
-            return m_store.size();
-
-        const MacAddress &mac = m_store.get(parent.internalId());
-        const StationPtr_t station = m_store.getStation(mac);
-
-        return station->getSSIDcount();
-    }
-
-    int columnCount(const QModelIndex & parent = QModelIndex()) const
-    {
-        if (parent.isValid())
-            return 1;
-
-        return 2;
-    }
-
-    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const
-    {
-        if (role != Qt::DisplayRole)
-            return QVariant();
-
-        if (index.internalId() == -1)
-        {
-            const MacAddress &mac = m_store.get(index.row());
-            if (index.column() == 0)
-            {
-                return QVariant(mac.toString());
-            }
-            else if (index.column() == 1)
-            {
-                return QVariant(mac.getManufacturer());
-            }
-            else
-            {
-                assert(false);
-            }
-        }
-        else
-        {
-            if (index.column() != 0)
-                return QVariant();
-
-            const MacAddress &mac = m_store.get(index.internalId());
-            const StationPtr_t station = m_store.getStation(mac);
-            return QVariant(station->getSSID(index.row()));
-        }
-    }
-
+    QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const;
+    QModelIndex parent(const QModelIndex &index) const;
+    int rowCount(const QModelIndex & parent = QModelIndex()) const;
+    int columnCount(const QModelIndex & parent = QModelIndex()) const;
+    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
     QVariant headerData(int section, Qt::Orientation orientation, int role) const;
 
 public slots:
